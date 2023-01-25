ff9489b8bdaebe2274972867871b5035c3211db7
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-01-25 16:51:32 +0700

    [MA] Navigation to another screen

---
+++ meal_app/lib/screens/meals_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+
+class MealsScreen extends StatefulWidget {
+  MealsScreen({super.key});
+
+  @override
+  State<MealsScreen> createState() => _MealsScreenState();
+}
+
+class _MealsScreenState extends State<MealsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meals'),
+      ),
+      body: Center(
+        child: Text('Navigation content'),
+      ),
+    );
+  }
+}
