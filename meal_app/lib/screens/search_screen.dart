ca66d89afada82c8ffc94b24ecaaa89e172385ba
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-01-31 16:02:14 +0700

    [MA] Switch widget on search screen

--- meal_app/lib/screens/search_screen.dart
+++ meal_app/lib/screens/search_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:meal_app/widgets/main_drawer.dart';
+
 
 class SearchScreen extends StatefulWidget {
   static const routeName = '/search';
@@ -11,14 +13,82 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+
+  bool _isGlutenFree = false;
+  bool _isVeganFree = false;
+  bool _isVegetarianFree = false;
+  bool _isLactoseFree = false;
+
+  Widget _buildSwitcher(bool currentValue, String title, String? description, Function(bool) updatedValueHandler ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description!),
+      value: currentValue, 
+      onChanged: updatedValueHandler,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Search'),
+        title: const Text('Search'),
       ),
-      body: Center(
-        child: Text('Search result'),
+      drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection',
+              style: Theme.of(context).textTheme.headlineSmall,
+            ),
+          ),
+          Expanded(
+            child: ListView(children: <Widget>[
+              _buildSwitcher(
+                _isGlutenFree,
+                'Gluten Free', 
+                'Only include Gluten Free meals.',
+                (newValue) {
+                  setState(() {
+                    _isGlutenFree = newValue;
+                  });
+                }
+              ),
+              _buildSwitcher(
+                _isVeganFree,
+                'Vegan Free', 
+                'Only include Vegan Free meals.',
+                (newValue) {
+                  setState(() {
+                    _isVeganFree = newValue;
+                  });
+                }
+              ),
+              _buildSwitcher(
+                _isVegetarianFree,
+                'Vegetarian Free', 
+                'Only include _Vegetarian Free meals.',
+                (newValue) {
+                  setState(() {
+                    _isVegetarianFree = newValue;
+                  });
+                }
+              ),
+              _buildSwitcher(
+                _isLactoseFree,
+                'Lactose Free', 
+                'Only include Lactose Free meals.',
+                (newValue) {
+                  setState(() {
+                    _isLactoseFree = newValue;
+                  });
+                }
+              ),
+            ],),
+          ),
+        ],
       ),
     );
   }
