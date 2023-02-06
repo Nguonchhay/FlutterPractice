1fe5fc7dff86a70722b99bb55462dd56314a9300
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-02-06 16:43:57 +0700

    [BS] Expand on order item

--- banana_shop/lib/widgets/order_item.dart
+++ banana_shop/lib/widgets/order_item.dart
@@ -1,13 +1,29 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 import 'package:banana_shop/states/models/order.dart';
 
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final Order order;
 
   OrderItem(this.order, {super.key});
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+
+  bool _isExpanded = false;
+
+  void toggleExpanse() {
+    setState(() {
+      _isExpanded = !_isExpanded;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,17 +32,40 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${order.amount}'),
+            title: Text('\$${widget.order.amount}'),
             subtitle: Text(
-              DateFormat('dd/MMM/yy hh:mm').format(order.dateTime)
+              DateFormat('dd/MMM/yy hh:mm').format(widget.order.dateTime)
             ),
             trailing: IconButton(
-              icon: const Icon(Icons.expand_more),
-              onPressed: () {
-
-              },
+              icon: Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: toggleExpanse,
             ),
           ),
+          if (_isExpanded)
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15, 
+                vertical: 10
+              ),
+              height: min(widget.order.products.length * 20.0 + 20, 180),
+              child: ListView(
+                children: widget.order.products.map(
+                  (product) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        product.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text('${product.quantity} * ${product.price}'),
+                    ],
+                  ),
+                ).toList(),
+              ),
+            ),
         ],
       ),
     );
