f4683c7ee5acbdf7ed3dd944069be0754d1e5af4
Nguonchhay Touch <nguonchhay.touch@gmail.com>
2023-11-22 17:31:32 +0700

    [SHOE] Add simple splash screen

---
+++ shoe_shop/lib/screens/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:shoe_shop/screens/home_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+
+  static const routeName = '/splash';
+
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        Navigator.pushReplacementNamed(context, HomeScreen.routeName);
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlutterLogo(
+              size: 100.0,
+            ),
+            SizedBox(height: 30,),
+            Text('Shoe Shop')
+          ],
+        ),
+      ),
+    );
+  }
+}
