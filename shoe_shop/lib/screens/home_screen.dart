f58d627d2e56c96eef4cde6a62b3479c97acdbf5
Touch Nguonchhay <nguonchhay.touch@gmail.com>
2024-01-08 11:42:15 +0700

    [SHOE_APP] Use font awesome

--- shoe_shop/lib/screens/home_screen.dart
+++ shoe_shop/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shoe_shop/screens/auth/login_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,22 +13,41 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed(LoginScreen.routeName);
-            }, 
-            icon: const Icon(Icons.shop),
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text('Content'),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(horizontal: 15.0),
+              child: Row(
+                children: [
+                  TextField(
+                    controller: searchController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                    ),
+                    style: const TextStyle(
+                      backgroundColor: Colors.lightBlue,
+                    ),
+                    
+                  ),
+                  IconButton(
+                    onPressed: () {
+
+                    }, 
+                    icon: const FaIcon(FontAwesomeIcons.cartShopping),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
